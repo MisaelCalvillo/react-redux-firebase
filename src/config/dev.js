091cfc0b03f295acec39190904fc34c188ae19e0
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-12-08 12:30:54 -0600

    Ya quedo

--- src/config/dev.js
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-  apiKey: "AIzaSyBdrFaaSvT3emmR3P74JQ9GRzh1RMTOX2A",
-  authDomain: "react-redux-firebase-a70cf.firebaseapp.com",
-  databaseURL: "https://react-redux-firebase-a70cf.firebaseio.com",
-  projectId: "react-redux-firebase-a70cf",
-  storageBucket: "react-redux-firebase-a70cf.appspot.com",
-  messagingSenderId: "758464258873"
-}
